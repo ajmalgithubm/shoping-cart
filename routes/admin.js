d11549ba0b3a4f4cbeaae7f1c36949cc62edf480
ajmalgithubm <ajmalm76774@gmail.com>
2023-08-05 23:57:45 +0530

    getAllproduct function is added

--- routes/admin.js
+++ routes/admin.js
@@ -3,16 +3,23 @@ const router = express.Router();
 const productHelpers = require('../helpers/productHelpers');
 
 
-const products = [
-    { name: 'Samsung', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQm_3hRPHJHkB-0At9kFInSWPiH1zusqbkjX9F2GzRexRokVboC4hiRQNI0XAXc3umb6x4&usqp=CAU' },
-    { name: 'Ralme', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTfYhz956tW3eRjph34xnmOD2sxiy1-bB33Sg&usqp=CAU' },
-    { name: 'Oppo', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUwSWaz1NYBViVEhqAnyoAK6P3ijTnOCAX2Q&usqp=CAU' },
-    { name: 'Iphone', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSe4LhkEVth4qzxUPYxd0GCeTkO0ZDJtT8Iug&usqp=CAU' }
-]
+// const products = [
+//     { name: 'Samsung', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQm_3hRPHJHkB-0At9kFInSWPiH1zusqbkjX9F2GzRexRokVboC4hiRQNI0XAXc3umb6x4&usqp=CAU' },
+//     { name: 'Ralme', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTfYhz956tW3eRjph34xnmOD2sxiy1-bB33Sg&usqp=CAU' },
+//     { name: 'Oppo', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUwSWaz1NYBViVEhqAnyoAK6P3ijTnOCAX2Q&usqp=CAU' },
+//     { name: 'Iphone', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSe4LhkEVth4qzxUPYxd0GCeTkO0ZDJtT8Iug&usqp=CAU' }
+// ]
 
 
 router.get('/', (req, res) => {
-    res.render('admin/view-product', {admin:true, title:"admin panel", products})
+    productHelpers.getAllProduct((products) =>{
+        if(products){
+            res.render('admin/view-product', { admin: true, title: "admin panel", products })
+        }else{
+            console.log("error occur when fetching data")
+        }
+    })
+    // 
 })
 
 router.get('/add-product', (req, res) => {
@@ -25,7 +32,13 @@ router.post('/add-product', (req, res) => {
             const id = result.insertedId.toString();
             req.files.image.mv('./public/productImages/'+id+'.jpg', (err, done) => {
                 if(!err){
-                    res.send('data added successfully')
+                    productHelpers.getAllProduct((products) => {
+                        if (products) {
+                            res.render('admin/view-product', { admin: true, title: "admin panel", products })
+                        } else {
+                            console.log("error occur when fetching data")
+                        }
+                    })
                 }else{
                     console.log("error occur when image storing");
                 }
