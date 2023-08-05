caf449dbc8d3e9f9f3a43940be8d70f862994cfd
ajmalgithubm <ajmalm76774@gmail.com>
2023-08-05 15:20:32 +0530

    add product admin panel is added

--- routes/admin.js
+++ routes/admin.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const router = express.Router();
+const products = [
+    { name: 'Samsung', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQm_3hRPHJHkB-0At9kFInSWPiH1zusqbkjX9F2GzRexRokVboC4hiRQNI0XAXc3umb6x4&usqp=CAU' },
+    { name: 'Ralme', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTfYhz956tW3eRjph34xnmOD2sxiy1-bB33Sg&usqp=CAU' },
+    { name: 'Oppo', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUwSWaz1NYBViVEhqAnyoAK6P3ijTnOCAX2Q&usqp=CAU' },
+    { name: 'Iphone', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSe4LhkEVth4qzxUPYxd0GCeTkO0ZDJtT8Iug&usqp=CAU' }
+]
+
 
 router.get('/', (req, res) => {
-    res.render('admin/admin', {admin:true, title:"admin panel"})
+    res.render('admin/admin', {admin:true, title:"admin panel", products})
+})
+
+router.get('/add-product', (req, res) => {
+   res.render('admin/add-product', {admin:true, title:"admin panel"})
 })
 
 
--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,15 @@
 const express = require('express')
-const router = express.Router()
+const router = express.Router();
+const products = [
+    { name: 'Samsung', image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQm_3hRPHJHkB-0At9kFInSWPiH1zusqbkjX9F2GzRexRokVboC4hiRQNI0XAXc3umb6x4&usqp=CAU'},
+    { name: 'Ralme', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTfYhz956tW3eRjph34xnmOD2sxiy1-bB33Sg&usqp=CAU' },
+    { name: 'Oppo', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUwSWaz1NYBViVEhqAnyoAK6P3ijTnOCAX2Q&usqp=CAU' },
+    { name: 'Iphone', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSe4LhkEVth4qzxUPYxd0GCeTkO0ZDJtT8Iug&usqp=CAU' }
+]
+
 
 router.get('/', (req,res) => {
-    res.render('user/products', {admin:false, title:'shoping cart'})
+    res.render('user/products', {admin:false, title:'shoping cart', products})
 })
 
 module.exports = router
